3a7cb46aa39507985d341fb7ddb9896ebdcd11a2
HaykoTsarukyan <avchyanraffi96@gmail.com>
2019-07-04 20:38:11 +0400

    bajanel enq maseri

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SideBar from './components/SideBar';
+import Content from './components/Content';
 import logo from './logo.svg';
 import './App.css';
+import { directive } from '@babel/types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    country : "Armenia"
+  };
+  constructor(props){
+    super(props);
+    this.handleOnChangeCountry = this.handleOnChangeCountry.bind(this);
+  }
+  handleOnChangeCountry(){
+    this.setState( state:{country:"USA"})
+  }
+  render(){
+    return(
+      <React.Fragment>
+        <p>{this.state.country}</p>
+        <button onClick={this.handleOnChangeCountry}>Poxel yerkir@</button>
+        </React.Fragment>
+    );
+  }
 }
-
 export default App;
