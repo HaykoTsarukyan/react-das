15d9de872874935a104ddb43e37979d9d938fb94
HaykoTsarukyan <hayktsarukyan94@gmail.com>
2019-07-06 18:00:36 +0400

    added components

--- src/App.js
+++ src/App.js
@@ -4,27 +4,28 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import SideBar from './components/SideBar';
 import Content from './components/Content';
-import logo from './logo.svg';
-import './App.css';
-import { directive } from '@babel/types';
 
-class App extends React.Component{
+import './App.css';
+
+class App extends React.Component {
   state = {
-    country : "Armenia"
+    country: "Armenia"
   };
-  constructor(props){
+  constructor(props) {
     super(props);
     this.handleOnChangeCountry = this.handleOnChangeCountry.bind(this);
   }
-  handleOnChangeCountry(){
-    this.setState( state:{country:"USA"})
+  handleOnChangeCountry() {
+    this.setState({ country: "USA" })
   }
-  render(){
-    return(
+  render() {
+    return (
       <React.Fragment>
-        <p>{this.state.country}</p>
-        <button onClick={this.handleOnChangeCountry}>Poxel yerkir@</button>
-        </React.Fragment>
+        <Header />
+        <SideBar />
+        <Content />
+        <Footer />
+      </React.Fragment>
     );
   }
 }
